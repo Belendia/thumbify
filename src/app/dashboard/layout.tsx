edeae8c8d1cb0c74336f6d56973721ae2174484b
Belendia <belendia@gmail.com>
2025-02-20 16:14:46 +0300

    add thumbnail creator

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,20 +14,20 @@ export default async function Layout({
 }: Readonly<{ children: React.ReactNode }>) {
   const serverSession = await auth();
 
-  // const user = await db.user.findUnique({
-  //   where: {
-  //     id: serverSession?.user.id,
-  //   },
-  //   select: {
-  //     credits: true,
-  //   },
-  // });
+  const user = await db.user.findUnique({
+    where: {
+      id: serverSession?.user.id,
+    },
+    select: {
+      credits: true,
+    },
+  });
 
   return (
     <div className="flex h-screen w-full flex-col items-center overflow-y-scroll px-6 py-6">
       <nav className="flex w-full items-center justify-end pb-6">
         <div className="flex items-center gap-4">
-          {/* <p>{user?.credits} credits left</p> */}
+          <p>{user?.credits} credits left</p>
           <Link href="/dashboard/pricing">
             <Button>Buy more</Button>
           </Link>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 "use server";
 
 import Link from "next/link";
-// import Recent from "~/components/recent";
-// import Style from "~/components/style";
-// import ThumbnailCreator from "~/components/thumbnail-creator";
+import Recent from "~/components/recent";
+import Style from "~/components/style";
+import ThumbnailCreator from "~/components/thumbnail-creator";
 import { Button } from "~/components/ui/button";
 import { env } from "~/env";
 import { auth } from "~/server/auth";
@@ -11,45 +11,43 @@ import { db } from "~/server/db";
 
 const Page = async () => {
   const serverSession = await auth();
-  // const user = await db.user.findUnique({
-  //   where: {
-  //     id: serverSession?.user.id,
-  //   },
-  //   select: {
-  //     credits: true,
-  //   },
-  // });
+  const user = await db.user.findUnique({
+    where: {
+      id: serverSession?.user.id,
+    },
+    select: {
+      credits: true,
+    },
+  });
 
   return (
     <div className="flex h-screen max-w-full items-center justify-center px-4 md:max-w-3xl md:px-0">
       <div className="flex max-w-full flex-col gap-10">
-        {
-          // user?.credits === 0 ? (
-          //   <div className="flex flex-col px-10 md:mt-10">
-          //     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          //       Hi there
-          //     </h1>
-          //     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          //       Want to create a thumbnail?
-          //     </h1>
-          //     <div className="mt-2 flex flex-col gap-3">
-          //       <p className="leading-7 text-muted-foreground">
-          //         Buy more credits to continue generating thumbnails.
-          //       </p>
-          //       <Link href="/dashboard/pricing">
-          //         <Button>Buy credits</Button>
-          //       </Link>
-          //     </div>
-          //     <div className="mt-8">
-          //       <Recent />
-          //     </div>
-          //   </div>
-          // ) : (
-          //   <ThumbnailCreator>
-          //     <Recent />
-          //   </ThumbnailCreator>
-          // )
-        }
+        {user?.credits === 0 ? (
+          <div className="flex flex-col px-10 md:mt-10">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              Hi there
+            </h1>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              Want to create a thumbnail?
+            </h1>
+            <div className="mt-2 flex flex-col gap-3">
+              <p className="leading-7 text-muted-foreground">
+                Buy more credits to continue generating thumbnails.
+              </p>
+              <Link href="/dashboard/pricing">
+                <Button>Buy credits</Button>
+              </Link>
+            </div>
+            <div className="mt-8">
+              <Recent />
+            </div>
+          </div>
+        ) : (
+          <ThumbnailCreator>
+            <Recent />
+          </ThumbnailCreator>
+        )}
       </div>
     </div>
   );
